c626a158573c023858e4e2f7255fffbaa35cb745
Cloudox <cloudox@foxmail.com>
2017-06-03 10:03:36 +0800

    实现效果

--- OXBlurDemo/ViewController.m
+++ OXBlurDemo/ViewController.m
@@ -16,7 +16,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    imageView.image = [UIImage imageNamed:@"neo.jpeg"];
+    [self.view addSubview:imageView];
+    
+    /* iOS 7以上支持，UIBarStyle枚举：
+     UIBarStyleDefault
+     UIBarStyleBlack
+     UIBarStyleBlackOpaque
+     UIBarStyleBlackTranslucent
+     */
+//    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, imageView.frame.size.height * 0.7, imageView.frame.size.width, imageView.frame.size.height * 0.3)];
+//    toolbar.barStyle = UIBarStyleBlackTranslucent;
+//    [self.view addSubview:toolbar];
+    
+    
+    /* iOS 8以上支持（推荐），UIBlurEffectStyle枚举
+     UIBlurEffectStyleRegular
+     UIBlurEffectStyleLight
+     UIBlurEffectStyleDark
+     UIBlurEffectStyleProminent
+     UIBlurEffectStyleExtraLight
+    */
+    UIBlurEffect *effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:effect];
+    effectView.frame = CGRectMake(0, imageView.frame.size.height * 0.7, imageView.frame.size.width, imageView.frame.size.height * 0.3);
+    [self.view addSubview:effectView];
 }
 
 
