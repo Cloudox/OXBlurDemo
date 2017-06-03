3dbece2fd3ff113a8e8708405e39c6f376f3505f
Cloudox <cloudox@foxmail.com>
2017-06-03 09:29:13 +0800

    Initial Commit

---
+++ OXBlurDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OXBlurDemo
+//
+//  Created by csdc-iMac on 2017/6/3.
+//  Copyright © 2017年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OXBlurDemoTests/OXBlurDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  OXBlurDemoTests.m
+//  OXBlurDemoTests
+//
+//  Created by csdc-iMac on 2017/6/3.
+//  Copyright © 2017年 Cloudox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OXBlurDemoTests : XCTestCase
+
+@end
+
+@implementation OXBlurDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
